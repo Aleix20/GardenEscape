f6ba82bad524cfc5063601b0a888790c204f7ec9
Aleix20 <aleix2077@gmail.com>
2022-05-07 08:20:20 +0200

    Pick up boxes with time
    We grab a box staying on top around 3 sec

--- Assets/scripts/grabController.cs
+++ Assets/scripts/grabController.cs
@@ -8,6 +8,11 @@ public class grabController : MonoBehaviour
     public Transform boxHolder;
     public float rayDist;
 
+    private Vector3  lastPosition;
+    private float waitTime;
+
+    private float fixTime = 3.0f;
+
     bool itemGrabbed = false;
     
 
@@ -17,22 +22,27 @@ public class grabController : MonoBehaviour
 
         RaycastHit grabCheck;
         Physics.Raycast(grabDetect.position, Vector3.down, out grabCheck ,rayDist );
+        Debug.Log("WaitTime is: " + waitTime);
         Debug.Log(grabCheck.collider.tag);
         if (grabCheck.collider != null  )
         {
-            if (grabCheck.collider.tag == "Box" && Input.GetKeyDown(KeyCode.Space) && !itemGrabbed)
+            if (grabCheck.collider.tag == "Box" && waitTime >= fixTime && !itemGrabbed)
             {
+                Debug.Log("Pick up box");
                 itemGrabbed = !itemGrabbed;
+                waitTime = 0.0f;
 
-            }else if (grabCheck.collider.tag == "Box" && Input.GetKeyDown(KeyCode.Space) && itemGrabbed)
+            }
+            else if (grabCheck.collider.tag == "Box" && waitTime >= fixTime && itemGrabbed)
             {
+                Debug.Log("Drop box");
                 itemGrabbed = !itemGrabbed;
+                waitTime = 0.0f;
             }
 
             
             if (itemGrabbed )
             {
-                Debug.Log("Pick up box");
                 grabCheck.collider.gameObject.transform.parent = boxHolder;
                 grabCheck.collider.gameObject.transform.position = boxHolder.position;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody>().isKinematic = true;
@@ -40,10 +50,26 @@ public class grabController : MonoBehaviour
             }
             else if(!itemGrabbed) 
             {
-                Debug.Log("Drop box");
+                
                 grabCheck.collider.gameObject.transform.parent = null;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody>().isKinematic = false;
             }
         }
+    }
+    void LateUpdate()
+    {
+
+        float distToLastPos = Vector3.Distance(grabDetect.position, lastPosition);
+
+        if (distToLastPos < 0.3f)
+        {
+            waitTime += Time.deltaTime;
+        }
+        else
+        {
+            waitTime = 0.0f;
+            lastPosition = grabDetect.position;
+        }
+
     }
 }
